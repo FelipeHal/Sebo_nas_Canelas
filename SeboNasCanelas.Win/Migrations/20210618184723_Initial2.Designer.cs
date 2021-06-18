c21d5e8f592da7582837284c2d7ba474548c1e7e
Luiz Felipe Sampaio da Fonseca <luizffons@hotmail.com>
2021-06-18 15:56:22 -0300

    Migrations Games Done

--- SeboNasCanelas.Win/Migrations/20210618184723_Initial2.Designer.cs
+++ SeboNasCanelas.Win/Migrations/20210618184723_Initial2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SeboNasCanelas.Win;
 
 namespace SeboNasCanelas.Win.Migrations
 {
     [DbContext(typeof(SeboNasCanelasContext))]
-    partial class SeboNasCanelasContextModelSnapshot : ModelSnapshot
+    [Migration("20210618184723_Initial2")]
+    partial class Initial2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SeboNasCanelas.Win/Migrations/20210618184723_Initial2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SeboNasCanelas.Win.Migrations
+{
+    public partial class Initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Developer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Console = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AgeRating = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StockQuantity = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    AddedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
